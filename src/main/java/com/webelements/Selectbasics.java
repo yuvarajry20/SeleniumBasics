eaca88e97195aaeef6f20f6de71112e0aa9b8869
yuvarajry20 <2k21cse187@kiot.ac.in>
2025-03-12 11:17:24 +0530

    commit

---
+++ src/main/java/com/webelements/Selectbasics.java
@@ -0,0 +1,32 @@
+package com.webelements;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Selectbasics {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://leafground.com/select.xhtml");
+		WebElement UIAutomation=driver.findElement(By.xpath("//select[@class='ui-selectonemenu']"));
+		Select dropdown=new Select(UIAutomation);
+		List<WebElement> list1=dropdown.getOptions();
+		System.out.println("The dropdown options size: "+list1.size());
+		for(WebElement is: list1) {
+			System.out.println("The drop down options are: "+is.getText());
+		}
+		boolean multiple=dropdown.isMultiple();
+		System.out.println("The multiple options available: "+multiple);
+		dropdown.selectByVisibleText("Puppeteer");
+		dropdown.selectByIndex(1);
+	}
+
+}
